0fd76c36ce05f204d0a4a8a04582824783ada10a
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2025-02-16 21:39:25 -0300

    dev: iniciando microsservicos

---
+++ logistica-entrega-microservice/src/main/java/com/fiap/logistica_entrega_microservice/LogisticaEntregaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.logistica_entrega_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaEntregaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaEntregaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ logistica-entrega-microservice/src/test/java/com/fiap/logistica_entrega_microservice/LogisticaEntregaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.logistica_entrega_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaEntregaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
