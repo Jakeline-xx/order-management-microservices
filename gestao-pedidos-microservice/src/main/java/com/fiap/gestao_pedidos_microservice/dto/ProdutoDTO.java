d6e54e5150cbb675bee3c185740bf1c4c8ba88df
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2025-02-18 01:04:44 -0300

    dev: teste para gestao de produto

--- gestao-pedidos-microservice/src/main/java/com/fiap/gestao_pedidos_microservice/dto/ProdutoDTO.java
+++ gestao-pedidos-microservice/src/main/java/com/fiap/gestao_pedidos_microservice/dto/ProdutoDTO.java
@@ -1,12 +1,10 @@
 package com.fiap.gestao_pedidos_microservice.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class ProdutoDTO {
     private String nome;
     private String descricao;
